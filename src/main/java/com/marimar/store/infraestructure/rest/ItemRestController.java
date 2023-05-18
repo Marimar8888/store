d0e15ac53a9c7ef5069c34a1ab2c5e61194fdcf8
Marimar <malonso@quadram.mobi>
2023-05-18 10:37:13 +0200

    Form binding visualizar un artículo en un formulario o mostrar formulario vacío para uno nuevo

--- src/main/java/com/marimar/store/infraestructure/rest/ItemRestController.java
+++ src/main/java/com/marimar/store/infraestructure/rest/ItemRestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ItemRestController {
@@ -52,6 +53,14 @@ public class ItemRestController {
         this.itemService.deleteItem(itemId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
-
-
+    @CrossOrigin
+    @GetMapping(value = "/items/{itemId}")
+    ResponseEntity<ItemDTO> getItemById(@PathVariable Long itemId){
+        Optional<ItemDTO> item = this.itemService.getItemById(itemId);
+        if(item.isPresent()){
+            return new ResponseEntity<>(item.get(), HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
 }
